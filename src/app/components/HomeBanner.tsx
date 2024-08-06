0f219a57ffe9b9cf54521a730ebbbd540e0d351c
Mitchell Thomas <mt@mt.codes>
2024-08-06 18:21:19 -0400

    initial importing of custom built elements

---
+++ src/app/components/HomeBanner.tsx
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+
+export default function HomeBanner() {
+    return (
+        <Fragment>
+            <div className="grid grid-cols-12">
+                <div className="col-span-6"><h1>Mitchell Thomas</h1></div>
+                <div className="col-span-6"><h1>Mitchell Thomas</h1></div>
+            </div>
+        </Fragment>
+    );
+}
---
+++ src/app/components/Satellite.tsx
@@ -0,0 +1,11 @@
+import { Fragment } from "react"
+
+export default function Satellite() {
+    return(
+        <Fragment>
+
+            <div className='container'>satellite</div>
+
+        </Fragment>
+    )
+}
